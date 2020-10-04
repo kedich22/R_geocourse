4c70e090d190a6a3d28f042863acc8c4cd327d08
Andrei Kedich <kedich22@gmail.com>
2020-10-04 23:30:46 +0300

    Правки дз

--- 4_Kedich.R
+++ 4_Kedich.R
@@ -2,35 +2,35 @@ library(tidyverse)
 library(readxl)
 library(writexl)
 
-soil <- read_xlsx('soil_data.xlsx', 1)
-desc <- read_xlsx('soil_data.xlsx', 2, col_names = F)
+soil <- read_xls('soil_data.xls', 1)
+desc <- read_xls('soil_data.xls', 2, col_names = F)
 colnames(desc) <- c('ID', 'desc')
+
+fill_status <- function(x) { #Функция для заполнения поля с качеств характеристикой
+  if (x == 100) {
+    status = 'полностью'
+  } else if (x == 0) {
+    status =  "не заполнена"
+  } else {
+    status = 'частично'
+  }
+  return(status)
+}
 
 # Создание функции для проверки заполненности ячеек
 check_completeness <- function(df) { 
-  completeness <- function(x) { #Функция внутр для определения процента заполненности
-    perc <- round((1 - (sum(is.na(x)) / length(x))) * 100, 0)
-    return(perc)
-  }
-  stat <- function(x) { #Функция внутренняя для заполнения поля с качеств характеристикой
-    if (x == 100) {
-      status = 'полностью'
-    } else if (x == 0) {
-      status =  "не заполнена"
-    } else {
-      status = 'частично'
-    }
-    return(status)
-  }
-  # Создание новой таблицы, обращение к созданным функциям
-  dfnew <- tibble('names' = colnames(df), 'desc' = desc[, 2], 'completeness' = sapply(df, completeness),
-                  'status' = sapply(completeness, stat))
+
+# Создание новой таблицы, обращение к созданным функциям
+  dfnew <- tibble('names' = colnames(df), 'completeness' 
+                  = sapply(df, function(x) {round((1 - (sum(is.na(x)) / length(x))) * 100, 0)}),
+                  'status' = sapply(completeness, fill_status))
   return(dfnew)
 }
 
 # Создание нового списка, разделение по таксонам почв и применение ф-ции
 newsoil <- soil %>% 
   split(soil$SOIL_ID) %>% 
-  lapply(check_completeness) 
+  lapply(check_completeness) %>% 
+  lapply(function(x) {mutate(x, 'desc' = desc$desc, .before = 2)})
 
 write_xlsx(newsoil, 'completeness.xlsx')
